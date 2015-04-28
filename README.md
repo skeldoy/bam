5cc07f2108c22c4c93120181132d4440fc198f88
skeldoy <skeldoy@gmail.com>
2015-04-28 20:05:48 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # bam
-Build all module - a small bootstrapping script for building puppet modules
+
+Build all modules - a small bootstrapping script for building puppet class skeletons. 
+
+Put this folder in your /usr/local/etc/puppet/modules catalog and go anywhere and make a test.pp with the following content:
+$bamList = ["puppetclass"]
+class {"bam":}
+
+If you now run that with puppet apply (future parser should be enabled in conf or you can use --parser future as a parameter to apply)
+you will see that it made a puppet class for you: puppetclass. This class is a skeleton that you can fill with puppet code. 
+
+./manifests/install.pp - code that explains how the software or service is installed (packages needed etc)
+./manifests/service.pp - code that describes the services that need to run or whatnot
+./manifests/params.pp - parameters for the module itself
+./manifests/init.pp - the thing that loads the class
+./manifests/config.pp - code that configures the thing you want to configure
+
+By separating the installation, service and config-code you will get a much more readable and reusable codebase. The classes
+even start looking good on graphs ;)
+
+bam takes an array as a parameter for classes to build - so you can build many skeletons if you like.
+
+Keep in mind that you might want to quickly change bam itself. Your organisation might already have a way of doing things - secret
+recipes that you might want to add to bam. I suggest using bam to build a class that you call "protoclasser" and use that to build a more advanced bam.
+And that is what bam is - the very first step of metaprogramming puppet.
+
+
---
+++ manifests/init.pp
@@ -0,0 +1,107 @@
+class bam {
+
+#$createModules = ["rabbitmq"]
+$createModules = $::bamList
+
+each ($createModules) |$metamodule| { 
+$description = "Please write a description"
+$params = ""
+file { "/usr/local/etc/puppet/modules/$metamodule":
+    ensure => "directory",
+}
+->
+file { "/usr/local/etc/puppet/modules/$metamodule/manifests":
+    ensure => "directory",
+}
+->
+file { "/usr/local/etc/puppet/modules/$metamodule/manifests/init.pp":
+	owner => root,
+	group => wheel,
+	mode => 444,
+	content => "
+class $metamodule (
+\$${metamodule}_name = ${metamodule}::params::${metamodule}_name,
+\$${metamodule}_description = ${metamodule}::params::${metamodule}_description,
+\$${metamodule}_parameters = ${metamodule}::params::${metamodule}_parameters,
+) inherits ${metamodule}::params {
+anchor {'${metamodule}::begin': } ->
+class {'${metamodule}::install': } ->
+class {'${metamodule}::config': } ~>
+class {'${metamodule}::service': } ->
+anchor { '${metamodule}::end': }
+}
+",
+}
+->
+file { "/usr/local/etc/puppet/modules/$metamodule/manifests/params.pp":
+        owner => root,
+        group => wheel,
+        mode => 444,
+        content => " 
+class ${metamodule}::params {
+\$${metamodule}_name = \"${metamodule}\" 
+\$${metamodule}_description = \"${description}\" 
+\$${metamodule}_parameters = \"${params}\"
+}
+",
+} # end of file
+->
+file { "/usr/local/etc/puppet/modules/$metamodule/manifests/config.pp":
+        owner => root,
+        group => wheel,
+        mode => 444,
+        content => "
+class ${metamodule}::config inherits ${metamodule} {
+# Now we configure ${metamodule}
+file { \"/usr/local/etc/${metamodule}\":
+ ensure => directory,
+}->
+file { \"/usr/local/etc/${metamodule}/${metamodule}.conf\":
+ owner => root,
+ group => wheel,
+ mode => 444,
+ source => \"puppet:///modules/${metamodule}/${metamodule}.conf\",
+ }
+}
+",
+} # end of file
+->
+file { "/usr/local/etc/puppet/modules/$metamodule/manifests/install.pp":
+        owner => root,
+        group => wheel,
+        mode => 444,
+        content => "
+class ${metamodule}::install inherits ${metamodule} {
+# Now we install stuff
+include pkgng
+package { \"${metamodule}\":
+ ensure => installed,
+ }
+
+
+}
+",
+} # end of file
+->
+file { "/usr/local/etc/puppet/modules/$metamodule/manifests/service.pp":
+        owner => root,
+        group => wheel,
+        mode => 444,
+        content => "
+class ${metamodule}::service inherits ${metamodule} {
+# conf up the rc-script and start the process
+service { \"${metamodule}\":
+ ensure => running,
+ enable => true
+}
+
+}
+",
+} # end of file
+
+}
+#end of each
+
+
+#end of class 
+}
